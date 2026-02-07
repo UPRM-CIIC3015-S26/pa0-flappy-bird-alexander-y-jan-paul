f0b52c44161d92a2540934d5d54b7a34edf852de
AlexanderCarril <alexander.carril@upr.edu>
2026-02-06 23:33:16 -0400

    Completed Tasks 1, 2.2, 4, and 6

--- main.py
+++ main.py
@@ -35,10 +35,10 @@ score_y = 10
 # Player Variables -->
 bird_x = 50
 bird_y = 300
-bird_velocity = 0
+bird_velocity = 0.5
 # TODO 1: Tweaking the physics
 # Looks like the player is falling too quickly not giving a change to flap it's wing, maybe tweak around with the value of this variable
-gravity = 9.81
+gravity =  0.8
 jump = -10
 # Pipe Variables -->
 pipe_x = 400
@@ -51,7 +51,7 @@ pipe_height = random.randint(100, 400)
 # TODO 2.2: The too fast problem
 # The pipes are moving way too fast! Play around with the pipe_speed variable until you find a good
 # speed for the player to play in!
-pipe_speed = 20
+pipe_speed = 6.5
 
 score = 0
 game_over = False
@@ -63,7 +63,7 @@ running = True
 while running:
     # TODO 6: Changing the name!
     # D'oh! This is not yout name isn't follow the detailed instructions on the PDF to complete this task.
-    name = "Homer Simpson"
+    name = "Alexander Carril Jordan"
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -97,7 +97,7 @@ while running:
             # TODO 4: Fixing the scoring
             # When you pass through the pipes the score should be updated to the current score + 1. Implement the
             # logic to accomplish this scoring system.
-            score = 1
+            score = score + 1
 
         if bird_y > 600 or bird_y < 0:
             game_over = True
